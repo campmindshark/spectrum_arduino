f12b735d60d652bc41145c8cc784438a8954ec52
Ashoat Tevosyan <ashoat@gmail.com>
2016-08-01 04:29:26 -0400

    New program

--- mindshark_leds.ino
+++ mindshark_leds.ino
@@ -23,62 +23,48 @@ void setup() {
   leds.show();
 }
 
-// 0: just initialized; don't know what mode we are in.
-//    this mode does nothing
-// 1: we are passed a set of commands in the form (pixel, color)
-//    and flush them to the LEDs whenever requested
-byte mode = 0;
-
-void loop() {
-  if (mode == 0) {
-    int new_mode = Serial.read();
-    if (new_mode <= 0) {
-      return;
-    }
-    mode = new_mode;
-  }
-  if (mode == 1) {
-    bool keep_going = mode1();
-    if (!keep_going) {
-      mode = 0;
-    }
-  }
-}
-
 // we wait for two bytes.
 //  if we get a 0, we exit (return false)
 //  if we get a 1, we flush
 //  if we get a n that is neither 0 nor 1, then:
 //   - LED index = n - 2
 //   - we wait for 3 bytes specifying color
-bool mode1() {
+void loop() {
   char first_bytes[2];
   int bytes_read = Serial.readBytes(first_bytes, 2);
   if (bytes_read != 2) {
-    // I'm not smart yet
-    return true;
+    // Still waiting...
+    return;
   }
   int first = first_bytes[0] | (first_bytes[1] << 8);
   if (first == 0) {
-    return false;
+    // This used to mean something
+    return;
   }
   if (first == 1) {
     if (!leds.busy()) {
       leds.show();
     }
-    return true;
+    return;
   }
   int LED_index = first - 2;
   char color_bytes[3];
   bytes_read = Serial.readBytes(color_bytes, 3);
   if (bytes_read != 3) {
-    // I'm not smart yet
-    return true;
+    // Should never happen, would be bad...
+    return;
   }
   int color = color_bytes[0]
     | (color_bytes[1] << 8)
     | (color_bytes[2] << 16);
   leds.setPixel(LED_index, color);
-  return true;
+  return;
+}
+
+void flashColor(int color) {
+  for (int i = 0; i < 8 * ledsPerStrip; i++) {
+    leds.setPixel(i, color);
+  }
+  leds.show();
 }
 
